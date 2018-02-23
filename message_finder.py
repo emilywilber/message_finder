51cc39b1e79b00e9358014374f5d576dcc93b2f9
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-02-23 13:46:27 -0500

    Add files via upload

--- message_finder.py
+++ message_finder.py
@@ -1,6 +1,37 @@
-with open('file_000010.txt', 'r') as f:
+for nums in range(0, 100):
+    name = "file_%d" % nums + ".txt"
+    if len(name) == 14:
+        name = "file_0%d" % nums + ".txt"
+    if len(name) == 13:
+        name = "file_00%d" % nums + ".txt"
+    if len(name) == 12:
+        name = "file_000%d" % nums + ".txt"
+    if len(name) == 11:
+        name = "file_0000%d" % nums + ".txt"
+    if len(name) == 10:
+        name = "file_00000%d" % nums + ".txt"
+    
+with open("/Users/ewilbe5944/project/snel/text_files/" + name, 'r') as f:
     readcontent = f.read()
     f.close()
+
+
+def mean(inputlist):
+    total = 0
+    for i in inputlist:
+        total += i
+    total = total / len(inputlist)
+    return total
+
+def chi_square(inputlist):
+    expected = mean(inputlist)
+    
+    X = 0
+
+    for n in inputlist:
+        X += ((n - expected)*(n - expected))/expected
+
+    return X
 
 counts = [0] * 127
 for c in readcontent:
@@ -8,4 +39,6 @@ for c in readcontent:
     counts[n] += 1
 counts = counts[32:127]
 
-print(counts)
+if chi_square(counts) > 100:
+     print(name)
+#ok so it works but it only print the last numbered file
